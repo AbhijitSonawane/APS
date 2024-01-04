91e619ec866214539e859a2d0f4070fa964b7dd9
AbhijitSonawane <92243830+AbhijitSonawane@users.noreply.github.com>
2024-01-04 15:24:05 +0530

    Update shellscript.sh

--- shellscript.sh
+++ shellscript.sh
@@ -7,4 +7,4 @@ echo $JOB_NAME
 echo "User has selected Region as $REGION_CHOICE"
 echo "User has selected Env as $ENV_CHOICE"
 echo "User has selected Env as $ENV_CHOICE"
-echo "This is simple command to check webhook"
+echo "This is simple command to check webhook."
